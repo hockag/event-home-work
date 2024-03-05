edb843cd1ad026b678bb2dfde7b4b5ea4a023f01
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-03-05 21:04:00 +0200

    save

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const openModal = document.querySelector('[data-action="open-modal"]');
 const closeModal = document.querySelector('[data-action="close-modal"]');
-
+const modal = document.querySelector('.modal');
