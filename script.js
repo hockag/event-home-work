db08e425f699eaa5cb7d9ddf40b5fe7265f9ea84
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-03-06 17:57:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ window.addEventListener("DOMContentLoaded",() => {
     const modal = document.querySelector('.modal');
     const backdrop = document.querySelector('.backdrop');
 
-    
+    openModal.addEventListener('click', toggleModal);
+    closeModal.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        backdrop.style.opacity = "1";
+        backdrop.style.visibility = "visible";
+    }
 
 })
