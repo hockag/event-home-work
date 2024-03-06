4babac0f46b318af4ea62f6ca12d1a225d29ae24
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-03-06 17:52:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const openModal = document.querySelector('[data-action="open-modal"]');
-const closeModal = document.querySelector('[data-action="close-modal"]');
-const modal = document.querySelector('.modal');
-
-
 window.addEventListener("DOMContentLoaded",() => {
     const openModal = document.querySelector('[data-action="open-modal"]');
-    const closeModal = document.querySelector('[data-action="close-modal"]')
+    const closeModal = document.querySelector('[data-action="close-modal"]');
+    const modal = document.querySelector('.modal');
+    const backdrop = document.querySelector('.backdrop');
+
+    
+
 })
